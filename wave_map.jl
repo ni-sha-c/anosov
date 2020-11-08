6ffbdd070664c7ac3690389f9f944457008d92e9
Nisha Chandramoorthy <nishac@mit.edu>
2020-11-08 16:29:46 -0500

    Correct error in wave map

--- wave_map.jl
+++ wave_map.jl
@@ -3,7 +3,7 @@ function wave_tent(x,s)
 		pert = sqrt( (1+s)^2 - 4*s*x)
 		return 4*x/(1 + s + pert)
 	end
-	pert = sqrt( (1-s)^2 - 4*s*(2-x))
+	pert = sqrt( (1-s)^2 + 4*s*(2-x))
 	return  4*(2-x)/(1 - s + pert)
 end
 
